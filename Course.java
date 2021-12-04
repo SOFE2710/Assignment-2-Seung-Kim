2e665da15219c0d039b9e3b800a089489e034307
seungkimsofe2710 <95396053+seungkimsofe2710@users.noreply.github.com>
2021-12-03 22:54:05 -0500

    Update Course.java

--- Course.java
+++ Course.java
@@ -1,1 +1,53 @@
+import java.util.Vector;
 
+public class Course{
+
+   private Department dept;
+   private String title; // title of the course (e.g. Intro to programming);
+   private String code; // course code, e.g. SOFE, ELEE, MANE, etc.
+   private int number; // course number, e.g. 1200, 2710, 2800, etc.
+   private Vector<Student> classList; // contains all students registered in this course
+
+   public Course(String code, int number, Department dept, String title) {
+	      // also initialize the classList;
+	 	   this.code=code;
+	 	   this.number=number;
+	 	   this.dept=dept;
+	 	   this.title=title;
+	 	   this.classList=new Vector<Student>();
+   }
+
+   
+   public Course() {
+	// TODO Auto-generated constructor stub
+       classList=new Vector<Student>();
+   }
+
+    public String getCode(){
+
+        return code;
+    }
+   public int getNumber(){
+       return number;
+   }
+
+   public Vector<Student> getStudents(){
+       return classList;
+   }
+
+   public void addStudentToCourse(Student std){
+
+       if(!classList.contains(std)){
+           classList.add(std);
+       }
+
+    }
+ 
+  public String toString() {
+     // return a string representation of the course with the course code,
+     // name, and number of people registered in the course in the following
+     // format:
+     // SOFE 2710 Object Oriented Programming and Design, Enrollment = 260
+
+     return code+" "+number+" "+title+", Enrollment = "+classList.size();
+  }
